3fe19639cc5ffcb15b64a806d74fabe676e35e93
thanh1831995 <lethanh1831995@gmail.com>
2017-06-05 14:28:09 +0700

    t

--- QuanLyKS/QuanLyKS/HuongDanSD.cs
+++ QuanLyKS/QuanLyKS/HuongDanSD.cs
@@ -16,5 +16,53 @@ namespace QuanLyKS
         {
             InitializeComponent();
         }
+
+        private void hDSDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\HDSD.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyPhongToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Quan Ly Phong.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Quan Ly Thue Phong.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyDichVuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Quan Ly Dich Vu.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyThietBiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Quan Ly Thiet Bi.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyKhachHangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Nhan Vien.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void quanLyNhanVienToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Quan Ly Nhan vien.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
+
+        private void hoaDonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file = "E:\\HDSD\\Hoa don.mhtml";
+            wbw_huongdan.Url = new Uri(file);
+        }
     }
 }
